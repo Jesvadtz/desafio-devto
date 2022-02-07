e33bb84ebc7219616e06920e77a184b36dffb263
RubS <rub.somuano.g@gmail.com>
2022-02-07 10:41:41 -0600

    Btn post coment

--- RubJames.js
+++ RubJames.js
@@ -1,25 +1,24 @@
-
 //contador de los corazones
 const korazon = document.getElementById("corazon");
 const contador1 = document.getElementById("kokoro");
 
 let i = 0;
-korazon.addEventListener("click",() => {
-    console.log( i++ )
-    contador1.innerHTML =  i;
-    console.log(i);
-   });
- 
+korazon.addEventListener("click", () => {
+  console.log(i++);
+  contador1.innerHTML = i;
+  console.log(i);
+});
+
 // contador de los unicornios
-   const unicornIo = document.getElementById("unicorn");
-   const unicornioCont = document.getElementById("unicorncont");
-   
-   let u = 0;
-   unicornIo.addEventListener("click",() => {
-       console.log( u++ )
-       unicornioCont.innerHTML =  u;
-       console.log(u);
-      });  
+const unicornIo = document.getElementById("unicorn");
+const unicornioCont = document.getElementById("unicorncont");
+
+let u = 0;
+unicornIo.addEventListener("click", () => {
+  console.log(u++);
+  unicornioCont.innerHTML = u;
+  console.log(u);
+});
 
 // contador de los guardados
 
@@ -27,8 +26,8 @@ const save = document.getElementById("guardar");
 const saveCont = document.getElementById("guardarcont");
 
 let s = 0;
-save.addEventListener("click",() => {
-    console.log( s++ )
-    saveCont.innerHTML =  s;
-    console.log(s);
-   });  
+save.addEventListener("click", () => {
+  console.log(s++);
+  saveCont.innerHTML = s;
+  console.log(s);
+});
