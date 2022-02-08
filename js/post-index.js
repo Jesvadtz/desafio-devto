8665f0e08c3603ecf1858f3740bf6a0ca220a893
Jessica <jesvadtz@gmail.com>
2022-02-08 02:17:13 -0600

    Print post in index.html

--- js/post-index.js
+++ js/post-index.js
@@ -1,5 +1,6 @@
 const endpoint = "https://api-devto-default-rtdb.firebaseio.com/posts.json";
 
+$("#postNewData").css("display", "none");
 $(document).ready(function () {
   let postList = () => {
     fetch(endpoint, {
@@ -15,14 +16,43 @@ $(document).ready(function () {
           let post = dataPost[postId];
           console.log(post);
 
+          // while (post.hasChildNodes()) {
+          //   post.removeChild(post.lastChild);
+          // }
+
           let titlePostList = post.title;
           let tagsPostList = post.tags;
-          let descPostList = post.formText;
           let datePostList = post.date;
 
-          $("#");
+          $("#postNewData").append($("<section/>"));
+          $("#postNewData").css("display", "block");
+          $("#titleNewData").text(titlePostList);
+          $("#dateNewData").text(
+            `Posted on ${new Date(datePostList).toLocaleString()}`
+          );
+          tagsPostList.forEach((tag) => {
+            $("#tagsNewData").append($("<a/>").text(tag));
+          });
         });
       });
   };
   postList();
+});
+
+//API images and names
+$(document).ready(function () {
+  fetch(`https://api.pexels.com/v1/search?query=nature&orientation=portrait`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let photoUser = data.photos[7].src.portrait;
+      let photoHeader = data.photos[1].src.portrait;
+      console.log(photoUser);
+      $("#imageNewData").attr({
+        src: `${photoUser}`,
+      });
+    });
+  namey.get(function (n) {
+    $("#userNewData").text(n);
+  });
 });
